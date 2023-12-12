744bb5264b1aa7067f69eddd13101521c413d0c7
Jakub Oleszczak <jakub.oleszczak@allegro.com>
2023-12-12 09:38:56 +0100

    day 4 part 2

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -2,54 +2,70 @@ import Algorithms
 import Foundation
 
 struct Day04: AdventDay {
-  struct Stratchcard {
-    let winningNumbers: Set<Int>
-    let yourNumbers: Set<Int>
-    
-    var value: Decimal {
-      let commonNumbersCount = winningNumbers.intersection(yourNumbers).count
-      return commonNumbersCount == 0 ? 0 : pow(2, commonNumbersCount - 1)
+    struct Stratchcard {
+        let winningNumbers: Set<Int>
+        let yourNumbers: Set<Int>
+        var count = 1
+        
+        var commonNumbersCount: Int {
+            winningNumbers.intersection(yourNumbers).count
+        }
+        
+        var value: Decimal {
+            return commonNumbersCount == 0 ? 0 : pow(2, commonNumbersCount - 1)
+        }
     }
-  }
-  
-  init(data: String) {
-    self.data = data
-    entities = data
-      .split(separator: "\n")
-      .map { String($0) }
-  }
-  
-  
-  // Save your data in a corresponding text file in the `Data` directory.
-  var data: String
-  
-  var entities: [String]
-  
-  // Replace this with your solution for the first part of the day's challenge.
-  func part1() -> Any {
-    entities
-      .map(ScratchcardParser.from(_:))
-      .reduce(into: 0) { partialResult, stratchcard in
-        partialResult += stratchcard.value
-      }
-  }
-  
-  // Replace this with your solution for the second part of the day's challenge.
-  func part2() -> Any {
-    "day 4 part 2"
-  }
+    
+    init(data: String) {
+        self.data = data
+        entities = data
+            .split(separator: "\n")
+            .map { String($0) }
+    }
+    
+    
+    // Save your data in a corresponding text file in the `Data` directory.
+    var data: String
+    
+    var entities: [String]
+    
+    // Replace this with your solution for the first part of the day's challenge.
+    func part1() -> Any {
+        entities
+            .map(ScratchcardParser.from(_:))
+            .reduce(into: 0) { partialResult, stratchcard in
+                partialResult += stratchcard.value
+            }
+    }
+    
+    // Replace this with your solution for the second part of the day's challenge.
+    func part2() -> Any {
+        var stratchCards = entities.map(ScratchcardParser.from(_:))
+        for i in stratchCards.indices {
+            let winningCardsCount = stratchCards[i].commonNumbersCount
+            guard winningCardsCount > 0 else {
+                continue
+            }
+            for j in ((i + 1)...(i + winningCardsCount)).clamped(to: ClosedRange(stratchCards.indices)) {
+                stratchCards[j].count += stratchCards[i].count
+            }
+        }
+        return stratchCards.reduce(into: 0) { partialResult, stratchCard in
+            partialResult += stratchCard.count
+        }
+    }
 }
 
 extension Day04 {
-  enum ScratchcardParser {
-    static func from(_ str: String) -> Day04.Stratchcard {
-      let initialSplit = str.split(separator: "|")
-      let leftSide = initialSplit.first!.split(separator: ":")
-      let winningNumbers = leftSide.last!.split(separator: " ")
-        .map { Int(String($0)) ?? -1 }
-      let yourNumbers = initialSplit.last!.split(separator: " ")
-        .map { Int(String($0)) ?? -1 }
-      return .init(winningNumbers: .init(winningNumbers), yourNumbers: .init(yourNumbers))
+    enum ScratchcardParser {
+        static func from(_ str: String) -> Day04.Stratchcard {
+            let initialSplit = str.split(separator: "|")
+            let leftSide = initialSplit.first!.split(separator: ":")
+            let winningNumbers = leftSide.last!.split(separator: " ")
+                .map { Int(String($0)) ?? -1 }
+            let yourNumbers = initialSplit.last!.split(separator: " ")
+                .map { Int(String($0)) ?? -1 }
+            return .init(winningNumbers: .init(winningNumbers), yourNumbers: .init(yourNumbers))
+        }
     }
-  }
 }
--- Tests/Day04.swift
+++ Tests/Day04.swift
@@ -19,76 +19,22 @@ final class Day04Tests: XCTestCase {
     let challenge = Day04(data: testData)
     XCTAssertEqual(String(describing: challenge.part1()), "13")
   }
-  
-  func testPart1_trailingSymbol() throws {
-    let aTestData = """
-      467..114..
-      ...*......
-      ..35..633.
-      .........%
-      617*......
-      .....+.58.
-      ..592.....
-      ......755.
-      ...$.*....
-      .664.598..
-      """
-    let challenge = Day04(data: aTestData)
-    XCTAssertEqual(String(describing: challenge.part1()), "4361")
-  }
-  
-  func testPart1_leadingSymbol() throws {
-    let aTestData = """
-      467..114..
-      ...*......
-      ..35..633.
-      .........%
-      617*......
-      .....+.58.
-      ..592.....
-      ......755.
-      /....*....
-      .664.598..
-      """
-    let challenge = Day04(data: aTestData)
-    XCTAssertEqual(String(describing: challenge.part1()), "4361")
-  }
-  
-  func testPart1_commonSymbol() throws {
-    let aTestData = """
-      467..114..
-      ...*......
-      ..35..633.
-      .........%
-      617*......
-      .....+.58.
-      ..592.....
-      ......755#
-      ....*.....
-      .664.598..
-      """
-    let challenge = Day04(data: aTestData)
-    XCTAssertEqual(String(describing: challenge.part1()), "4361")
-  }
-  
-  func testPart1_symbolInTheMiddle() throws {
-    let aTestData = """
-      .111*222..
-      """
-    let challenge = Day04(data: aTestData)
-    XCTAssertEqual(String(describing: challenge.part1()), "333")
-  }
 
   func testPart2() throws {
     let challenge = Day04(data: testData)
-    XCTAssertEqual(String(describing: challenge.part2()), "467835")
+    XCTAssertEqual(String(describing: challenge.part2()), "30")
   }
-  
-  func testParsingGameId() {
-    XCTAssertTrue(true)
-  }
-  
-  func testParsingCube() {
-//    XCTAssertEqual(Day04.GameParser.cubes(from: "3 red"), Day04.Game.Cubes(count: 3, color: .red))
-  }
+    
+    func testPart2_penultimateWinningLine() throws {
+        let aTestData = """
+          Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+          Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+          Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+          Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+          Card 5: 87 83 26 28 32 | 88 30 70 32 93 22 82 36
+          Card 6: 31 18 13 56 74 | 70 77 10 23 35 67 36 11
+          """
+        let challenge = Day04(data: aTestData)
+        XCTAssertEqual(String(describing: challenge.part2()), "44")
+    }
 }
